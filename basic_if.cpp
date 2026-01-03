1d72021377d535573ac2ba11b8746f1a6bc3eceb
Sam Arshad <jsb211@txstate.edu>
2026-01-02 20:20:59 -0600

    A really basic if condition

---
+++ basic_if.cpp
@@ -0,0 +1,20 @@
+/*
+Write an if statement that performs the following logic: if the variable x is equal to 20, then assign 0 to the variable y.
+*/
+
+#include <iostream> 
+using namespace std;
+
+int main(){
+ int x = 21, y = 2;  //random
+
+ if (x > 20){
+  y = 0;
+ }
+ 
+ cout << x << endl <<  y << endl;
+
+
+ return 0;
+
+}
